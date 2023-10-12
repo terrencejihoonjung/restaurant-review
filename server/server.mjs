25e0b7f32a697b1bf4a7088a168c24e0efca6ee0
terrencejihoonjung <terrencejung928@gmail.com>
2023-10-11 22:38:45 -0700

    setting up database connection pool

--- server/server.mjs
+++ server/server.mjs
@@ -1,12 +1,15 @@
 import express from "express";
 import morgan from "morgan";
+import pool from "./db/index.mjs";
 const app = express();
 
 app.use(express.json()); // built-in body-parser
 app.use(morgan("dev")); // third-party logger
 
-app.get("/restaurants", (req, res) => {
-  res.send("this is a get response");
+app.get("/restaurants", async (req, res) => {
+  const body = await pool.query("SELECT * FROM restaurants");
+  console.log(body.rows);
+  res.send("got restaurants");
 });
 
 app.get("/restaurants/:id", (req, res) => {
