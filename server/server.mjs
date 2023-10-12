7d671ae3c9331e934f52fe85a5aa57ec30f835ed
terrencejihoonjung <terrencejung928@gmail.com>
2023-10-11 19:30:44 -0700

    configuring routes, set up body-parser and logging middleware

--- server/server.mjs
+++ server/server.mjs
@@ -1,8 +1,28 @@
 import express from "express";
+import morgan from "morgan";
 const app = express();
+
+app.use(express.json()); // built-in body-parser
+app.use(morgan("dev")); // third-party logger
 
 app.get("/restaurants", (req, res) => {
   res.send("this is a get response");
+});
+
+app.get("/restaurants/:id", (req, res) => {
+  res.send("this is a get response");
+});
+
+app.post("/restaurants", (req, res) => {
+  res.send("this is a post response");
+});
+
+app.put("/restaurants/:id", (req, res) => {
+  res.send("this is a put response");
+});
+
+app.delete("/restaurants/:id", (req, res) => {
+  console.log("this is a delete response");
 });
 
 const PORT = process.env.PORT || 3000;
