baff4489a0052e495cb0b41f14c92b40f4318970
terrencejihoonjung <terrencejung928@gmail.com>
2023-10-17 02:38:57 -0400

    working on detail page

--- client/src/routes/RestaurantDetail.tsx
+++ client/src/routes/RestaurantDetail.tsx
@@ -1,7 +1,38 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Restaurant } from "../context/RestaurantsContext";
+
 function RestaurantDetail() {
+  const { id } = useParams();
+  const [selectedRestaurant, setSelectedRestaurant] = useState<Restaurant>(
+    {} as Restaurant
+  );
+
+  console.log(id);
+
+  async function getRestaurant() {
+    try {
+      const response = await fetch(`http://localhost:3000/restaurants/${id}`, {
+        method: "GET",
+      });
+      const jsonData = await response.json();
+      setSelectedRestaurant(jsonData);
+    } catch (err) {
+      if (err instanceof Error) {
+        console.error(err);
+      } else {
+        console.error(err);
+      }
+    }
+  }
+
+  useEffect(() => {
+    getRestaurant();
+  }, []);
+
   return (
     <>
-      <div>RestaurantDetail</div>
+      <div>{selectedRestaurant.name}</div>
     </>
   );
 }
