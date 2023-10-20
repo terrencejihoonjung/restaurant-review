340eca761a17eda5037255d3b76b2b6f04353f5c
terrencejihoonjung <terrencejung928@gmail.com>
2023-10-20 14:44:49 -0400

    got rid of useMemo

--- client/src/components/RestaurantList.tsx
+++ client/src/components/RestaurantList.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useMemo } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useRestaurantsContext } from "../context/RestaurantsContext";
 import StarRating from "./StarRating";
